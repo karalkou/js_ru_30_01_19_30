028195e3f0b826b911c4eb8c543266eec6fb79c9
Karalkou <yury.karalkou@gmail.com>
2017-05-11 07:20:15 +0300

    Add ready homework (hw1)

--- src/CommentsBlock.js
+++ src/CommentsBlock.js
@@ -2,24 +2,43 @@ import React, {Component} from 'react'
 import Comment from './Comment'
 
 export default class CommentsBlock extends Component {
+    state={
+        isOpen: false
+    };
+
     render(){
+        let buttonText = 'show/hide';
+
+        return (
+            <div>
+                <button onClick={this.handleClick}>{this.state.isOpen ? 'Hide' : 'Show'}</button>
+                <ul>
+                    {this.getBody()}
+                </ul>
+            </div>
+
+        )
+    };
+
+    handleClick = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    };
+
+    getBody() {
+        if (!this.state.isOpen) return null;
+
         const {article} = this.props;
         const comments = article.comments;
         const commentsElements = comments.map((comment) => <li key={comment.id}>
             <Comment comment={comment}/>
         </li>);
 
-        let buttonText = 'show/hide';
-
         return (
-            <div>
-                <button>{buttonText}</button>
-                <ul>
-                    {commentsElements}
-                </ul>
-            </div>
-
+            <ul>
+                {commentsElements}
+            </ul>
         )
-    }
-
+    };
 }

004477d51a4900cb6d0f70312bc47e1481d6b1fa
Karalkou <yury.karalkou@gmail.com>
2017-05-15 22:18:52 +0300

    Add ht 3.2 (hw3)

--- src/components/DateRange.js
+++ src/components/DateRange.js
@@ -1,34 +1,50 @@
 import React, { Component } from 'react'
+import moment from 'moment';
 import DayPicker, { DateUtils } from 'react-day-picker'
 
 import 'react-day-picker/lib/style.css';
 
-class DateRange extends Component {
-
+export default class DateRange extends React.Component {
     state = {
         from: null,
-        to: null
-    }
-
-    handleDayClick = (e, day) => {
-        this.setState(DateUtils.addDayToRange(day, this.state))
-    }
-
+        to: null,
+    };
+    handleDayClick = day => {
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    };
+    handleResetClick = e => {
+        e.preventDefault();
+        this.setState({
+            from: null,
+            to: null,
+        });
+    };
     render() {
         const { from, to } = this.state;
-        const selectedRange = from && to && `${from.toDateString()} - ${to.toDateString()}`
         return (
-            <div className="date-range">
+            <div className="RangeExample">
+                {!from && !to && <p>Please select the <strong>first day</strong>.</p>}
+                {from && !to && <p>Please select the <strong>last day</strong>.</p>}
+                {from &&
+                to &&
+                <p>
+                    You chose from
+                    {' '}
+                    {moment(from).format('L')}
+                    {' '}
+                    to
+                    {' '}
+                    {moment(to).format('L')}
+                    .
+                    {' '}<a href="." onClick={this.handleResetClick}>Reset</a>
+                </p>}
                 <DayPicker
-                    ref="daypicker"
-                    selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
-                    onDayClick={ this.handleDayClick }
+                    numberOfMonths={1}
+                    selectedDays={[from, { from, to }]}
+                    onDayClick={this.handleDayClick}
                 />
-                {selectedRange}
             </div>
         );
     }
-
 }
-
-export default DateRange
